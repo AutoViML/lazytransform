9268b4ac8631cfcff8bea4fdb7875b2b2f12cee9
AutoViML <rsesha2001@yahoo.com>
2022-07-11 08:59:35 -0400

    Minor bug fixes. SuloRegressor added.

--- lazytransform.py
+++ lazytransform.py
@@ -1992,15 +1992,16 @@ def check_if_GPU_exists():
         return False
 ############################################################################################
 module_type = 'Running' if  __name__ == "__main__" else 'Imported'
-version_number =  '0.78'
-print(f"""{module_type} LazyTransformer version:{version_number}. Call by using:
-    lazy = LazyTransformer(model=None, encoders='auto', scalers=None, date_to_string=False,
-        transform_target=False, imbalanced=False, save=False, combine_rare=False, verbose=0)
-    ### if you are not using a model in pipeline, you must use fit and transform ###
-        X_trainm, y_trainm = lazy.fit_transform(X_train, y_train)
-        X_testm = lazy.transform(X_test)
-    ### If using a model in pipeline, use fit and predict only ###
-        lazy.fit(X_train, y_train)
-        lazy.predict(X_test)
-""")
+version_number =  '0.79'
+if module_type != 'Imported':
+    print(f"""{module_type} LazyTransformer version:{version_number}. Call by using:
+        lazy = LazyTransformer(model=None, encoders='auto', scalers=None, date_to_string=False,
+            transform_target=False, imbalanced=False, save=False, combine_rare=False, verbose=0)
+        ### if you are not using a model in pipeline, you must use fit and transform ###
+            X_trainm, y_trainm = lazy.fit_transform(X_train, y_train)
+            X_testm = lazy.transform(X_test)
+        ### If using a model in pipeline, use fit and predict only ###
+            lazy.fit(X_train, y_train)
+            lazy.predict(X_test)
+    """)
 #################################################################################
